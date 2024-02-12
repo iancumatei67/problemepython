89ad28543b07057ec11490946e9c0a3ea94d140f
iancumatei67 <127155074+iancumatei67@users.noreply.github.com>
2024-02-12 10:02:52 +0200

    Problema 5

--- systeminfo.py
+++ systeminfo.py
@@ -1,4 +1,8 @@
 #Exercitiul 5
+#Write a script that gets system information like distro info, memory(total, used, free), CPU info (model, core numbers, speed), 
+#current user, system load average, and IP address. Use arguments for specifying resources.
+#(For example, -d for distro -m for memory, -c for CPU, -u for user info, -l for load average, -i for IP address).
+
 
 import argparse
 import os
