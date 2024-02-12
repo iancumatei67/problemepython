d73be59f8026a111e8eed7ee9f10143130b3c7e1
iancumatei67 <127155074+iancumatei67@users.noreply.github.com>
2024-02-12 10:01:54 +0200

    Problema 4

--- occurences.py
+++ occurences.py
@@ -1,4 +1,5 @@
 #Exercitiul 4
+#Given an input string, count occurrences of all characters within a string (e.g. pythonnohtyppy -> p:3, y:3, t:2, h:2, o:2, n:2).
 
 from collections import Counter
 
